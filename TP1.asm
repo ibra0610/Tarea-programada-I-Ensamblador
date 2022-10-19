1960cac0fcd11cf5d9ae412a137b8ad44fb1708f
Javier <ibrahimcruz06@gmail.com>
2022-10-18 22:53:06 -0600

    Se crea el menu

--- TP1.asm
+++ TP1.asm
@@ -2,111 +2,207 @@
 ;Lenguaje ensamblador Grupo 01
 
 .model small 
-.stack 
-.data
-    
-    salto db 10,13,7 
-    texto1 db 10,13,7, "1.AND", "$"
-    texto2 db 10,13,7, "2.OR", "$"
-    texto3 db 10,13,7, "3.NOT", "$"
-    texto4 db 10,13,7, "4.XOR", "$"
-    texto5 db 10,13,7, "5.SALIR", "$"  
-    texto6 db 10,13,7, "Ingrese su opcion", "$" 
-    
-    opcion db 0 
-    dato1 db 1 
-    dato2 db 2 
-    dato3 db 3
-    dato4 db 4
-    dato5 db 5 
-    
-    solicitud db 10,13,7, "Ingrese un entero hexadecimal: ", "$"
-    hex1 db 0 
-    hex2 db 0 
-    resultado db 0 
-    
-    mostrar db 10,13,7, "El resultado es: ", "$" 
+.stack 521 
+
+.data 
+
+    menu db "a.AND b.OR c.NOT d.XOR e.Salir"
+    eleccion db ?  
+    scancode1 db 30 
+    scancode2 db 48 
+    scancode3 db 46 
+    scancode4 db 32 
+    scancode5 db 18 
+    peticion db "Ingrese el primer numero hexadecimal: " 
+    peticion2 db "Ingrese el segundo numero hexadecimal: "
+    resultado db "El resultado de la operacion es: " 
+    fila db ? 
+    columna db ? 
+    hexa1 db 0 
+    hexa2 db 0  
     
 .code 
 
+
+    
+
+borre_pantalla PROC     NEAR  
+    
+    mov ax, 0600h 
+    mov bh, 07h 
+    mov cx, 0000h 
+    mov dx, 184Fh 
+    int 10h 
+    RET 
+    
+borre_pantalla ENDP 
+
+coloque_cursor PROC     NEAR 
+
+    mov ah, 02h 
+    mov bh, 0 
+    mov dh, fila 
+    mov dl, columna 
+    int 10h 
+    RET
+    
+coloque_cursor endp 
+
+
+obtenga_caracter PROC   NEAR 
+    
+    mov ah, 10h 
+    int 16h 
+    RET
+    
+obtenga_caracter ENDP 
+
+lea_teclado PROC    NEAR 
+
+    mov ah, 00h 
+    int 16h 
+    RET  
+lea_teclado ENDP
+
+
+Begin: 
+
     mov ax, @data 
     mov ds, ax 
+    mov ax, 0B800h 
+    mov es, ax 
+  
+    mov ah, 0 
+    mov al, 3 
+    int 10h 
+  
+call borre_pantalla 
+
+    mov ah, 07 
+    mov cx, 22 
+    mov si, offset menu 
+    mov di, 1620 
+    cld 
+
+imprima: 
     
-    imprima: 
-    mov ah, 09h
-    lea dx, salte
-    int 21h
+    lodsb 
+    stosw 
+    loop imprima 
     
-    mov ah, 09h
-    lea dx, texto1 
-    int 21h  
+    mov fila, 10 
+    mov columna, 39 
+ call coloque_cursor 
+ 
+    mov ax, @data 
+    mov es, ax 
     
-    mov ah, 09h
-    lea dx, texto2 
-    int 21h 
+    mov cx, 1000 
+    mov di, offset menu 
+
+lectura: 
     
-    mov ah, 09h
-    lea dx, texto3 
-    int 21h 
+    call lea_teclado 
     
-    mov ah, 09h
-    lea dx, texto4 
-    int 21h 
+    cmp ah, scancode1 
+    JE op_and  
     
-    mov ah, 09h
-    lea dx, texto5 
-    int 21h 
-    
-    mov ah, 09h
-    lea dx, texto6 
-    int 21h  
-    
-    obtener: 
-    mov ah, 01h 
-    int 21h 
-    sub al, 30h 
-    mov opcion, al 
-    
-    evalue:
-    cmp al, dato1
-    JE op_and 
-   
-    cmp al, dato2
+    cmp ah, scancode2 
     JE op_or 
-   
-    cmp al, dato3
+    
+    cmp ah, scancode3 
     JE op_not 
-   
-    cmp al, dato4
-    JE op_xor 
-   
-    cmp al, dato5
-    JE op_salir  
     
-    op_and: 
+    cmp ah, scancode4 
+    JE op_xor
     
+    cmp ah, scancode5 
+    JE op_salir
+        
+
+op_and:
+
+    mov ah, 0Ah 
+    mov al, 'A' 
+    mov bh,0 
+    mov cx,1 
+    int 10h  
+    call imprima 
+    call lectura
+
+op_or: 
+
+    mov ah, 0Ah 
+    mov al, 'B' 
+    mov bh,0 
+    mov cx,1 
+    int 10h 
+    call imprima 
+    call lectura
+
+
+op_not: 
+
+    mov ah, 0Ah 
+    mov al, 'C' 
+    mov bh,0 
+    mov cx,1 
+    int 10h 
+    call imprima 
+    call lectura
+
+op_xor: 
+    mov ah, 0Ah 
+    mov al, 'D' 
+    mov bh,0 
+    mov cx,1 
+    int 10h 
+    call imprima 
+    call lectura
     
-    op_or: 
+op_salir: 
+    mov ah, 0Ah 
+    mov al, 'E' 
+    mov bh,0 
+    mov cx,1 
+    int 10h
+    call imprima 
+    call lectura
+
+
+end Begin  
+
+ 
     
-    
-    op_not: 
-    
-    
-    op_xor: 
-     
-    
-    op_salir: 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
